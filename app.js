a42fac78b6f78656fef1358acba532a08f96e21e
FreeZaxe <freezaxe4@gmail.com>
2021-05-25 22:58:22 +0100

    fix page 404

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+
+
 const chargement = document.querySelector('.chargement');
 
 window.addEventListener('load', () => {
 
     chargement.classList.add('fondu-out');
-
+    setTimeout(() => { chargement.classList.add('zIndexNegatif'); }, 1600)
 })
