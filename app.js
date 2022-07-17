2ce117d3184ffee3b2816dfe266b7427789de4fc
FreeZaxe <freezaxe4@gmail.com>
2022-07-17 11:21:21 +0100

    On change tout

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// -------- CHARGEMENT --------
 
 const chargement = document.querySelector('.chargement');
 
@@ -7,3 +7,20 @@ window.addEventListener('load', () => {
     chargement.classList.add('fondu-out');
     setTimeout(() => { chargement.classList.add('zIndexNegatif'); }, 700)
 })
+
+
+// ------- ANIMATION --------
+
+window.addEventListener('scroll', () => { 
+    var reveals = document.querySelectorAll(".projet");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("actif");
+        }
+    }
+});
